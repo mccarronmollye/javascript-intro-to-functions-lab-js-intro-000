291315f962e8647ebbe45517be4793153dba5db5
mccarronmollye@gmail.com <mccarronmollye@gmail.com>
2018-07-30 13:07:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,12 @@ function logWhisper(string) {
 }
 
  function sayHiToGrandma(string) {
-   if (lowercase.toLowerCase() === lowercase) {
-    return "I can't hear you!"
-  }
-}
+   if (string === string.toLowerCase()) {
+     return "I can't hear you!"
+   } else if (string === string.toUpperCase()){
+     return "YES INDEED!"
+   } else if (string === "I love you, Grandma.") {
+     return "Ilove you, too."
+   }
+ }
+ 
